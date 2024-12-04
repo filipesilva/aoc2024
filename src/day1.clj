b9375029fd6f81738233ee14fdbecc8c25ef5e6d
Filipe Silva <filipematossilva@gmail.com>
2024-12-04 16:22:55 +0000

    day1 part2

--- src/day1.clj
+++ src/day1.clj
@@ -11,26 +11,39 @@
 (defn read-numbers
   [numbers-line]
   (->> (str/split numbers-line #"\s+")
-       (map parse-double)))
+       (map parse-double)
+       (map int)))
 
 (defn into-accs
   [[acc1 acc2] [n1 n2]]
   [(conj acc1 n1) (conj acc2 n2)])
 
+(defn read-all [rdr]
+  (->> rdr
+       line-seq
+       doall ;; not needed after adding reduce
+       ;; (take 100)
+       (map read-numbers)
+       (reduce into-accs [[] []])))
 
 (comment
+  ;; part 1
   (with-open [rdr (io/reader (io/resource "day1/input.txt"))]
     (->> rdr
-         line-seq
-         ;; doall ;; not needed after adding reduce
-         ;; (take 10)
-         (map read-numbers)
-         (reduce into-accs [[] []])
+         read-all
          (map sort)
          (apply map -)
          (map abs)
-         (reduce +)
-         int))
+         (reduce +)))
   ;; => 1506483
 
+
+  ;; part 2
+  (with-open [rdr (io/reader (io/resource "day1/input.txt"))]
+    (let [[one two] (read-all rdr)
+          freqs     (frequencies two)]
+      (->> one
+           (map #(* %(get freqs % 0)))
+           (reduce +))))
+  ;; => 23126924
   ,)
